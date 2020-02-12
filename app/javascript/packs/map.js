788dd15f0a1a941f23dcd89abc57d2cacb149168
atsede selassie <atsede@gmail.com>
2020-02-12 21:35:21 +0000

    added map with markers

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -16,6 +16,17 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
+
+    const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1500 });
+      };
+
+      if (mapElement) {
+        // [ ... ]
+        fitMapToMarkers(map, markers);
+      }
   }
 }
 
